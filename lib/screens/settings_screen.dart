c908a08402e001cab48b2cdadc1846bb6e4e7447
Anil Yadav <anil.yadav@vtransgroup.com>
2026-05-07 11:02:05 +0530

    Version 1.0.8 (8)

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -576,7 +576,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
               const SizedBox(height: 24),
               const Center(
                 child: Text(
-                  'Version 1.0.7 (7)',
+                  'Version 1.0.8 (8)',
                   style: TextStyle(color: Color(0xFF94A3B8), fontSize: 12),
                 ),
               ),
